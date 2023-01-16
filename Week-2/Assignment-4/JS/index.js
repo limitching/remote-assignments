3f9891f3485ba2736cb23d8de5d272cce3b4a3d1
HUNG WEI-CHING <hwc0320@gmail.com>
2023-01-16 09:52:36 +0800

    Week-2 Assignment 4 & 6: Improve UX

--- Week-2/Assignment-4/JS/index.js
+++ Week-2/Assignment-4/JS/index.js
@@ -1,9 +1,10 @@
+let textContainer = document.querySelector("section.welcome");
 let welcomeText = document.querySelector("section.welcome h1");
 let btn_action = document.querySelector("a.action");
 let newBoxes = document.querySelector("div.boxes2");
 //when clicked, display "Have a Good Time!"
-welcomeText.addEventListener("click", (e) => {
-  e.target.innerHTML = '<i lang="la">Have a Good Time!</i>';
+textContainer.addEventListener("click", (e) => {
+  welcomeText.innerHTML = '<i lang="la">Have a Good Time!</i>';
 });
 //when btn was clicked, change attribute "isHide" to false
 btn_action.addEventListener("click", (e) => {
