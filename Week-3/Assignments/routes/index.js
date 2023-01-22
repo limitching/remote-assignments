21c952b3c02d95bbe57c3cf1d4901804010ffd18
HUNG WEI-CHING <hwc0320@gmail.com>
2023-01-22 11:15:58 +0800

    Add link for Assignment-5

--- Week-3/Assignments/routes/index.js
+++ Week-3/Assignments/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Assignment 1" });
+  res.render("index", { title: "Assignments" });
 });
 
 module.exports = router;
