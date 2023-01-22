3a58657e8c0aad3dd061b246abec772e620f094a
HUNG WEI-CHING <hwc0320@gmail.com>
2023-01-22 11:35:02 +0800

    Week-2 Assignment-5 update

--- Week-2/Assignment-5/app.js
+++ Week-2/Assignment-5/app.js
@@ -3,19 +3,24 @@ function binarySearchPosition(numbers, target) {
   let start = 0;
   let end = numbers.length - 1;
 
-  while (start !== end) {
+  while (start <= end) {
     let check = numbers[Math.floor((start + end) / 2)];
     if (target > check) {
       //if target > check, update the starting search index
-      start = Math.floor((start + end) / 2);
+      start = Math.floor((start + end) / 2) + 1;
     } else if (target < check) {
       //if target > check, update the ending search index
-      end = Math.floor((start + end) / 2);
+      end = Math.floor((start + end) / 2) - 1;
     } else {
       //if target = check, return the index value of check
       return Math.floor((start + end) / 2);
     }
   }
+  return -1;
 }
 console.log(binarySearchPosition([1, 2, 5, 6, 7], 1)); // should print 0
 console.log(binarySearchPosition([1, 2, 5, 6, 7], 6)); // should print 3
+//New test case from instructor
+console.log(binarySearchPosition([1, 2, 5, 6, 7], 7)); // should print 4
+console.log(binarySearchPosition([1, 2, 5, 6, 7], -1)); // should print -1
+console.log(binarySearchPosition([1, 2, 5, 6, 7], 9)); // should print -1
